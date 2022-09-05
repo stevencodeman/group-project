13249f87d951406943655a8f290503d185b8e4d4
stevencodeman <stevencodeman@gmail.com>
2022-09-05 10:10:38 -0700

    css restored

--- main.js
+++ main.js
@@ -1,175 +1,118 @@
-let todos = JSON.parse(localStorage.getItem('todos')) || [];
-
-/*
-    added this to store the data for the running app
-    currently, just putting the `filter` value in here
-*/
-const state = {
-  filter: 'all',
-};
-
 window.addEventListener('load', () => {
-  const nameInput = document.querySelector('#name');
-  const newTodoForm = document.querySelector('#new-todo-form');
+	todos = JSON.parse(localStorage.getItem('todos')) || [];
+	const nameInput = document.querySelector('#name');
+	const newTodoForm = document.querySelector('#new-todo-form');
 
-  // filter toggle button
-  const filterCompleteButton = document.getElementById('filterComplete');
+	const username = localStorage.getItem('username') || '';
 
-  // on click for the button
-  filterCompleteButton.addEventListener('click', (e) => {
-    e.preventDefault();
+	nameInput.value = username;
 
-    // using this switch for the different filter states
-    switch (state.filter) {
-      // the default branch should never hit, but if we add a filter state
-      // and forget to handle that case, it's going to throw
-      // not great, but cheap and easy and we can "fix" it later
-      default: {
-        throw new Error(`You forgot filter state for "${state.filter}`);
-      }
-      // order of filter toggle is "all" -> "complete" -> "incomplete"
-      case 'all': {
-        state.filter = 'complete';
-        filterCompleteButton.innerText = 'Show Incomplete';
-        break; // breaks out of the `switch` statement without fall through
-      }
-      case 'complete': {
-        state.filter = 'incomplete';
-        break;
-      }
-      case 'incomplete': {
-        state.filter = 'all';
-        break;
-      }
-    }
+	nameInput.addEventListener('change', (e) => {
+		localStorage.setItem('username', e.target.value);
+	})
 
-    // call `displayTodos` to update the view on page
-    displayTodos();
-  });
+	newTodoForm.addEventListener('submit', e => {
+		e.preventDefault();
 
-  const username = localStorage.getItem('username') || '';
+		const todo = {
+			content: e.target.elements.content.value,
+			category: e.target.elements.category.value,
+			done: false,
+			createdAt: new Date().getTime()
+		}
 
-  nameInput.value = username;
+		todos.push(todo);
 
-  nameInput.addEventListener('change', (e) => {
-    localStorage.setItem('username', e.target.value);
-  });
+		localStorage.setItem('todos', JSON.stringify(todos));
 
-  newTodoForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+		// Reset the form
+		e.target.reset();
 
-    const todo = {
-      content: e.target.elements.content.value,
-      category: e.target.elements.category.value,
-      done: false,
-      createdAt: new Date().getTime(),
-    };
+		DisplayTodos()
+	})
 
-    todos.push(todo);
+	DisplayTodos()
+})
 
-    localStorage.setItem('todos', JSON.stringify(todos));
+function DisplayTodos () {
+	const todoList = document.querySelector('#todo-list');
+	todoList.innerHTML = "";
 
-    // Reset the form
-    e.target.reset();
+	todos.forEach(todo => {
+		const todoItem = document.createElement('div');
+		todoItem.classList.add('todo-item');
 
-    displayTodos();
-  });
+		const label = document.createElement('label');
+		const input = document.createElement('input');
+		const span = document.createElement('span');
+		const content = document.createElement('div');
+		const actions = document.createElement('div');
+		const edit = document.createElement('button');
+		const deleteButton = document.createElement('button');
 
-  displayTodos();
-});
+		input.type = 'checkbox';
+		input.checked = todo.done;
+		span.classList.add('bubble');
+		if (todo.category == 'other') {
+			span.classList.add('other');
+		} else {
+			span.classList.add('work');
+		}
+		content.classList.add('todo-content');
+		actions.classList.add('actions');
+		edit.classList.add('edit');
+		deleteButton.classList.add('delete');
 
-function displayTodos() {
-  const todoList = document.querySelector('#todo-list');
-  todoList.innerHTML = '';
+		content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+		edit.innerHTML = 'Edit';
+		deleteButton.innerHTML = 'Delete';
 
-  // filter the todos by `done` property
-  const filteredTodos = todos.filter((todo) => {
-    switch (state.filter) {
-      default:
-        throw new Error(`You forgot to handle filter "${state.filter}"`);
-      case 'all': // always return true because we're keeping all
-        return true;
-      case 'complete': // keep if the `done` prop is `true`
-        return todo.done;
-      case 'incomplete': // opposite of "complete"
-        return !todo.done;
-    }
-  });
+		label.appendChild(input);
+		label.appendChild(span);
+		actions.appendChild(edit);
+		actions.appendChild(deleteButton);
+		todoItem.appendChild(label);
+		todoItem.appendChild(content);
+		todoItem.appendChild(actions);
 
-  // replace "global" `todos` with our filtered copy
-  filteredTodos.forEach((todo) => {
-    const todoItem = document.createElement('div');
-    todoItem.classList.add('todo-item');
+		todoList.appendChild(todoItem);
 
-    const label = document.createElement('label');
-    const input = document.createElement('input');
-    const span = document.createElement('span');
-    const content = document.createElement('div');
-    const actions = document.createElement('div');
-    const edit = document.createElement('button');
-    const deleteButton = document.createElement('button');
+		if (todo.done) {
+			todoItem.classList.add('done');
+		}
+		
+		input.addEventListener('change', (e) => {
+			todo.done = e.target.checked;
+			localStorage.setItem('todos', JSON.stringify(todos));
 
-    input.type = 'checkbox';
-    input.checked = todo.done;
-    span.classList.add('bubble');
-    if (todo.category == 'other') {
-      span.classList.add('other');
-    } else {
-      span.classList.add('work');
-    }
-    content.classList.add('todo-content');
-    actions.classList.add('actions');
-    edit.classList.add('edit');
-    deleteButton.classList.add('delete');
+			if (todo.done) {
+				todoItem.classList.add('done');
+			} else {
+				todoItem.classList.remove('done');
+			}
 
-    content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
-    edit.innerHTML = 'Edit';
-    deleteButton.innerHTML = 'Delete';
+			DisplayTodos()
 
-    label.appendChild(input);
-    label.appendChild(span);
-    actions.appendChild(edit);
-    actions.appendChild(deleteButton);
-    todoItem.appendChild(label);
-    todoItem.appendChild(content);
-    todoItem.appendChild(actions);
+		})
 
-    todoList.appendChild(todoItem);
+		edit.addEventListener('click', (e) => {
+			const input = content.querySelector('input');
+			input.removeAttribute('readonly');
+			input.focus();
+			input.addEventListener('blur', (e) => {
+				input.setAttribute('readonly', true);
+				todo.content = e.target.value;
+				localStorage.setItem('todos', JSON.stringify(todos));
+				DisplayTodos()
 
-    if (todo.done) {
-      todoItem.classList.add('done');
-    }
+			})
+		})
 
-    input.addEventListener('change', (e) => {
-      todo.done = e.target.checked;
-      localStorage.setItem('todos', JSON.stringify(todos));
+		deleteButton.addEventListener('click', (e) => {
+			todos = todos.filter(t => t != todo);
+			localStorage.setItem('todos', JSON.stringify(todos));
+			DisplayTodos()
+		})
 
-      if (todo.done) {
-        todoItem.classList.add('done');
-      } else {
-        todoItem.classList.remove('done');
-      }
-
-      displayTodos();
-    });
-
-    edit.addEventListener('click', (e) => {
-      const input = content.querySelector('input');
-      input.removeAttribute('readonly');
-      input.focus();
-      input.addEventListener('blur', (e) => {
-        input.setAttribute('readonly', true);
-        todo.content = e.target.value;
-        localStorage.setItem('todos', JSON.stringify(todos));
-        displayTodos();
-      });
-    });
-
-    deleteButton.addEventListener('click', (e) => {
-      // use triple equals
-      todos = todos.filter((t) => t !== todo);
-      localStorage.setItem('todos', JSON.stringify(todos));
-      displayTodos();
-    });
-  });
+	})
 }
